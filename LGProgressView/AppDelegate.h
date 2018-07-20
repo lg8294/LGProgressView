34d18841ddbc4bff4bfccb603a7cbf0982464c01
Arrcen-LG <515930926@qq.com>
2018-07-20 10:35:48 +0800

    Initial Commit

---
+++ LGProgressView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LGProgressView
+//
+//  Created by Arrcen-LG on 2018/7/20.
+//  Copyright © 2018年 Arrcen-LG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LGProgressView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LGProgressView
+//
+//  Created by Arrcen-LG on 2018/7/20.
+//  Copyright © 2018年 Arrcen-LG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
